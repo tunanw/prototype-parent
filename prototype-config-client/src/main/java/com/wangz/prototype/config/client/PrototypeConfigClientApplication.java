4e73a89558d208c2f3e6e7078285176f7b13b254
wangzhuang <zhuang_wang@kingdee.com>
2019-08-09 15:31:55 +0800

    spring cloud bus

--- prototype-config-client/src/main/java/com/wangz/prototype/config/client/PrototypeConfigClientApplication.java
+++ prototype-config-client/src/main/java/com/wangz/prototype/config/client/PrototypeConfigClientApplication.java
@@ -2,12 +2,10 @@ package com.wangz.prototype.config.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-@RefreshScope
 public class PrototypeConfigClientApplication {
 
     public static void main(String[] args) {
